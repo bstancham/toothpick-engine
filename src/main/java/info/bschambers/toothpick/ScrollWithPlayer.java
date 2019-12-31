40cedfd855d20e22cfc10cbbbd4c11275d87472b
bschambers <ben@bschambers.info>
2019-12-31 11:02:30 +0000

    New ProgramBehaviour: ScrollWithPlayer

---
+++ src/main/java/info/bschambers/toothpick/ScrollWithPlayer.java
@@ -0,0 +1,29 @@
+package info.bschambers.toothpick;
+
+import info.bschambers.toothpick.actor.TPActor;
+
+public class ScrollWithPlayer implements ProgramBehaviour {
+
+    private TPActor player = null;
+
+    @Override
+    public void update(TPProgram prog) {
+
+        // find player
+        if (player == null)
+            if (prog.getPlayer().getActor().isAlive())
+                player = prog.getPlayer().getActor();
+
+        // update scrolling
+        if (player != null) {
+            TPGeometry g = prog.getGeometry();
+            g.xOffset = -player.x;
+            g.yOffset = -player.y;
+
+            // don't keep following a dead player
+            if (!player.isAlive())
+                player = null;
+        }
+    }
+
+}
